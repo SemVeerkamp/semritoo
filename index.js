293f849df466b3b62fb49d00a3fc1d7528d7fa6d
SemVeerkamp <98215962+SemVeerkamp@users.noreply.github.com>
2022-01-24 19:01:14 +0100

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-function deletePrediction(predictionId) {
-  fetch("/delete-prediction", {
-    method: "POST",
-    body: JSON.stringify({ predictionId: predictionId }),
-  }).then((_res) => {
-    window.location.href = "/";
-  });
-}
