1e4c9974df43f41be77157d1ca1a85bc03e78edc
SemVeerkamp <98215962+SemVeerkamp@users.noreply.github.com>
2022-01-24 19:03:04 +0100

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -1,5 +1,5 @@
 import os 
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = 'postgresql://wbaipmlhagglvr:dc75aed2c33a809bd118910f2170bdbaf6d21f6c9158acf4fa3ad93c79a58c37@ec2-63-32-7-190.eu-west-1.compute.amazonaws.com:5432/df6t7emeu38drt'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
