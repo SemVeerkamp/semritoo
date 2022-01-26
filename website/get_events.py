62ae62d29956c32fe2db20e56dd9549838a84671
SemVeerkamp <98215962+SemVeerkamp@users.noreply.github.com>
2022-01-26 12:31:52 +0100

    Update get_events.py

--- website/get_events.py
+++ website/get_events.py
@@ -2,8 +2,11 @@ from website.get_event_code import get_event_code
 import requests
 import json
 from datetime import datetime
+from datetime import timedelta
 
 def get_events(year,tag):
+    time_difference_hours = 9
+    time_difference_minutes = 28
     event_code = get_event_code(year, tag)
     url_events='https://api.isuresults.eu/events/'+event_code+'/competitions/'
     response_events = requests.get(url_events)
@@ -15,6 +18,7 @@ def get_events(year,tag):
         events.append(response_dict[i]['title'])
         scheduleNumbers.append(str(response_dict[i]['scheduleNumber']))
         date = datetime.strptime(response_dict[i]['start'], '%Y-%m-%dT%H:%M:%SZ')
+        date = date + timedelta(hours=time_difference_hours,minutes=time_difference_minutes)
         starttimes.append(date)
     return scheduleNumbers, events, starttimes
 
