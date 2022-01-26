58d42b36f1d218c005b9138d1e4f6366576d0e16
SemVeerkamp <98215962+SemVeerkamp@users.noreply.github.com>
2022-01-26 16:50:21 +0100

    Update get_result.py

--- website/get_result.py
+++ website/get_result.py
@@ -21,52 +21,56 @@ def get_result(year, tag):
     podium_event = []
 
     for i in range(len(scheduleNumbers)):
-        response_result = requests.get('https://api.isuresults.eu/events/'
-                                       + event_code
-                                       + '/competitions/'
-                                       + scheduleNumbers[i]
-                                       + '/results/'
-                                       )
-        response_result_dict = json.loads(response_result.text)
-        for j in range(len(response_result_dict)):
-            if response_result_dict[j]:
-                key = 'competitor'
-                if key in response_result_dict[j]:
-                    if response_result_dict[j]['time'] is not None:
-
-                        Full_name = str(response_result_dict[j]['competitor']['skater']['firstName']
-                                        + " "
-                                        + response_result_dict[j]['competitor']['skater']['lastName']
-                                        + "   ("
-                                        + response_result_dict[j]['competitor']['skater']['country']
-                                        + ")"
-                                        )
-                        time = str(response_result_dict[j]['time']
-                                   )
-                        if j < 3:
-                            podium_url = response_result_dict[j]['competitor']['skater']['photo']
-                            podium_event.append(podium_url)
-                            podium_url = None
-
-                        names.append(Full_name)
-                        times.append(time)
-                    else:
+        quarter = "Quarter"
+        semi = "Semi"
+        if quarter not in events[i]:
+            if semi not in events[i]:
+                response_result = requests.get('https://api.isuresults.eu/events/'
+                                               + event_code
+                                               + '/competitions/'
+                                               + scheduleNumbers[i]
+                                               + '/results/'
+                                               )
+                response_result_dict = json.loads(response_result.text)
+                for j in range(len(response_result_dict)):
+                    if response_result_dict[j]:
                         key = 'competitor'
                         if key in response_result_dict[j]:
-                            Full_name = str(response_result_dict[j]['competitor']['skater']['firstName']
-                                            + " "
-                                            + response_result_dict[j]['competitor']['skater']['lastName']
-                                            + "   ("
-                                            + response_result_dict[j]['competitor']['skater']['country']
-                                            + ") "
-                                            )
-                            time = "geen tijd"
-                            names.append(Full_name)
-                            times.append(time)
+                            if response_result_dict[j]['time'] is not None:
 
-                    results[str("startlist_" + events[i])] = names
-                    result_times[str("startlist_" + events[i])] = times
-                    podium_pictures[str("startlist_" + events[i])] = podium_event
+                                Full_name = str(response_result_dict[j]['competitor']['skater']['firstName']
+                                                + " "
+                                                + response_result_dict[j]['competitor']['skater']['lastName']
+                                                + "   ("
+                                                + response_result_dict[j]['competitor']['skater']['country']
+                                                + ")"
+                                                )
+                                time = str(response_result_dict[j]['time']
+                                           )
+                                if j < 3:
+                                    podium_url = response_result_dict[j]['competitor']['skater']['photo']
+                                    podium_event.append(podium_url)
+                                    podium_url = None
+
+                                names.append(Full_name)
+                                times.append(time)
+                            else:
+                                key = 'competitor'
+                                if key in response_result_dict[j]:
+                                    Full_name = str(response_result_dict[j]['competitor']['skater']['firstName']
+                                                    + " "
+                                                    + response_result_dict[j]['competitor']['skater']['lastName']
+                                                    + "   ("
+                                                    + response_result_dict[j]['competitor']['skater']['country']
+                                                    + ") "
+                                                    )
+                                    time = "geen tijd"
+                                    names.append(Full_name)
+                                    times.append(time)
+
+                            results[str("startlist_" + events[i])] = names
+                            result_times[str("startlist_" + events[i])] = times
+                            podium_pictures[str("startlist_" + events[i])] = podium_event
         names = []
         podium_event = []
         times = []
