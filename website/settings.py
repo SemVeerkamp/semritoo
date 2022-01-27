9645f68b8aa670a01d4e870142b81532e09041ba
SemVeerkamp <98215962+SemVeerkamp@users.noreply.github.com>
2022-01-27 13:58:12 +0100

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -1,5 +1,6 @@
 import os 
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://zqcwshnuvmsssb:bb31875ede38ab52448a4ea61f393a66fa4633b1523942663d3b96b7de40003f@ec2-79-125-93-182.eu-west-1.compute.amazonaws.com:5432/d2q6l6u8irtla1'
+heroku_uri = os.environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = heroku_uri[:8]+'ql'+heroku_uri[8:]
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
