bfdb421ef8254c810de2cff576d62df7b0b877ac
SemVeerkamp <98215962+SemVeerkamp@users.noreply.github.com>
2022-02-19 10:43:44 +0100

    Add files via upload

--- website/get_result.py
+++ website/get_result.py
@@ -35,8 +35,8 @@ def get_result(year, tag):
                 for j in range(len(response_result_dict)):
                     if response_result_dict[j]:
                         key = 'competitor'
-                        if response_result_dict[j]['time'] is not None:
-                            if key in response_result_dict[j]:
+#                        if response_result_dict[j]['time'] is not None:
+                        if key in response_result_dict[j]:
                                 if response_result_dict[j]['time'] is not None:
 
                                     Full_name = str(response_result_dict[j]['competitor']['skater']['lastName']
@@ -46,6 +46,10 @@ def get_result(year, tag):
     #                                                + response_result_dict[j]['competitor']['skater']['country']
     #                                                + ")"
                                                     )
+                                    if Full_name == "Lee Seung-Hoon":
+                                        Full_name = "LEE Seung Hoon"
+                                    if Full_name == "Chung Jaewon":
+                                        Full_name = "CHUNG Jae Won"
                                     time = str(response_result_dict[j]['time']
                                                )
                                     if j < 3:
@@ -65,13 +69,26 @@ def get_result(year, tag):
 #                                                       + response_result_dict[j]['competitor']['skater']['country']
 #                                                       + ") "
                                                         )
+                                        if Full_name == "Lee Seung-Hoon":
+                                            Full_name = "LEE Seung Hoon"
+                                        if Full_name == "Chung Jaewon":
+                                            Full_name = "CHUNG Jae Won"
                                         time = "geen tijd"
                                         names.append(Full_name)
                                         times.append(time)
-                                init_event = events[i].partition('0m')
-                                results[str(init_event[2]+'-s-'+init_event[0]+init_event[1]+'.htm').lower()] = names
-                                result_times[str(init_event[2]+'-s-'+init_event[0]+init_event[1]+'.htm').lower()] = times
-                                podium_pictures[str(init_event[2]+'-s-'+init_event[0]+init_event[1]+'.htm').lower()] = podium_event
+                                if i == 20:
+                                    results['men-s-mass-start.htm'] = names
+                                    result_times['men-s-mass-start.htm'] = times
+                                    podium_pictures['men-s-mass-start.htm'] = podium_event
+                                elif i == 21:
+                                    results['women-s-mass-start.htm'] = names
+                                    result_times['women-s-mass-start.htm'] = times
+                                    podium_pictures['women-s-mass-start.htm'] = podium_event
+                                else:
+                                    init_event = events[i].partition('0m')
+                                    results[str(init_event[2]+'-s-'+init_event[0]+init_event[1]+'.htm').lower()] = names
+                                    result_times[str(init_event[2]+'-s-'+init_event[0]+init_event[1]+'.htm').lower()] = times
+                                    podium_pictures[str(init_event[2]+'-s-'+init_event[0]+init_event[1]+'.htm').lower()] = podium_event
         names = []
         podium_event = []
         times = []
